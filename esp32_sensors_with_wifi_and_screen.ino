6470af3bcc6a5d6439b5632bea86841e17d1d97f
Alex Servirog <servalex1@gmail.com>
2020-04-04 23:52:18 +0300

    moved display code to a separate file

--- esp32_sensors_with_wifi_and_screen.ino
+++ esp32_sensors_with_wifi_and_screen.ino
@@ -1,41 +1,11 @@
 
 /////// display includes and defines
-#include <TFT_eSPI.h>
-#include <SPI.h>
-#include "WiFi.h"
-#include <Wire.h>
-#include "esp_adc_cal.h"
-
-#ifndef TFT_DISPOFF
-#define TFT_DISPOFF 0x28
-#endif
-
-#ifndef TFT_SLPIN
-#define TFT_SLPIN   0x10
-#endif
-
-#define TFT_MOSI            19
-#define TFT_SCLK            18
-#define TFT_CS              5
-#define TFT_DC              16
-#define TFT_RST             23
-
-#define TFT_BL          4  // Display backlight control pin
-#define ADC_EN          14
-#define ADC_PIN         34
-
-#define BUTTON_1 0
-#define BUTTON_2 35
-
-TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
-
-char buff[512];
+#include "display_part.h"
 ///////
 
 #include <Wire.h>
 #include <Adafruit_SHT31.h>
 #include <BH1750FVI.h>
-
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
@@ -49,6 +19,9 @@ BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
 #include "networkCredentials.h"
 
 /////// buttons
+#define BUTTON_1 0
+#define BUTTON_2 35
+
 int repeatStart = 1000;
 int repeatPeriod = 300;
 
@@ -107,16 +80,6 @@ void report()
   //tft.drawCircle(35, tft.height()/2-30 + currentMenu*30, 3, TFT_GREEN);
 }
 
-void clearScreen()
-{
-  tft.fillScreen(TFT_BLACK);
-}
-
-void writeCenter(String str)
-{
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.drawString(str, tft.width() / 2, tft.height() / 2);
-}
 /////// screen
 
 
@@ -124,19 +87,7 @@ void setup()
 {
     delay(1000);
 
-    tft.init();
-    tft.setRotation(1);
-    tft.fillScreen(TFT_BLACK);
-    tft.setTextSize(2);
-    //  tft.setTextColor(TFT_WHITE);
-    tft.setCursor(0, 0);
-    tft.setTextDatum(MC_DATUM);
-    tft.setTextSize(2);
-
-    if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-        pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
-        digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-    }
+    displaySetup();
 
     Serial.begin(115200);
     Serial.println("Start");
